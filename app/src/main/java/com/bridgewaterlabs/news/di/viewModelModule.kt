e8db8a1da28246a5660f2fd39c6d1bc4ff48b6ae
Orlovic982 <milanorlovic1982@gmail.com>
2022-03-25 13:59:06 +0100

    Adding BWL_NEWS fragment

--- app/src/main/java/com/bridgewaterlabs/news/di/viewModelModule.kt
+++ app/src/main/java/com/bridgewaterlabs/news/di/viewModelModule.kt
@@ -1,5 +1,6 @@
 package com.bridgewaterlabs.news.di
 
+import com.bridgewaterlabs.news.ui.main.home.HomeViewModel
 import com.bridgewaterlabs.news.ui.onboarding.forgotpassword.ForgotPasswordViewModel
 import com.bridgewaterlabs.news.ui.onboarding.register.RegisterViewModel
 import com.bridgewaterlabs.news.ui.onboarding.resetpassword.ResetPasswordViewModel
@@ -10,4 +11,5 @@ val viewModelModule = module {
     viewModel { ForgotPasswordViewModel() }
     viewModel { RegisterViewModel() }
     viewModel { ResetPasswordViewModel() }
+    viewModel { HomeViewModel() }
 }
--- app/src/main/java/com/bridgewaterlabs/news/ui/common/successregister/SuccessRegisterDialogFragment.kt
+++ app/src/main/java/com/bridgewaterlabs/news/ui/common/successregister/SuccessRegisterDialogFragment.kt
@@ -1,11 +1,13 @@
 package com.bridgewaterlabs.news.ui.common.successregister
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import com.bridgewaterlabs.news.databinding.DialogFragmentSuccesRegisterBinding
+import com.bridgewaterlabs.news.ui.main.MainActivity
 
 class SuccessRegisterDialogFragment : DialogFragment() {
 
@@ -15,6 +17,13 @@ class SuccessRegisterDialogFragment : DialogFragment() {
         savedInstanceState: Bundle?
     ): View? {
         val binding = DialogFragmentSuccesRegisterBinding.inflate(inflater, container, false)
+        binding.btnGonext.setOnClickListener {
+            Intent(requireContext(), MainActivity::class.java).also {
+                startActivity(it)
+            }
+
+        }
+
         return binding.root
     }
 }
